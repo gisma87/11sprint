eb4736a0477a624f9e0d4f55b321dc0a2c2f2814
stivman <gisma87@yandex.ru>
2020-05-26 02:45:22 +0700

    add project

---
+++ src/index.js
@@ -0,0 +1,70 @@
+import "./style.css";
+import {Popup} from './js/Popup.js';
+import {Api} from './js/Api.js';
+import {Card} from './js/Card.js';
+import {CardList} from './js/CardList.js';
+import {FormValidator} from './js/FormValidator.js';
+import {PopupAvatar} from './js/PopupAvatar.js';
+import {PopupCard} from './js/PopupCard.js';
+import {PopupImage} from './js/PopupImage.js';
+import {PopupProfile} from './js/PopupProfile.js';
+import {UserInfo} from './js/UserInfo.js';
+
+const container = document.querySelector('.places-list');
+const userPhoto = document.querySelector('.user-info__photo');
+const errorMessages = {
+    valueMissing: 'Это обязательное поле',
+    tooShort: 'Должно быть от 2 до 30 символов',
+    typeMismatch: 'Здесь должна быть ссылка'
+};
+const popupNewCard = document.querySelector('#popupNewPlace');
+const userInfoName = document.querySelector('.user-info__name');
+const userInfoJob = document.querySelector('.user-info__job');
+const popupProfileNode = document.querySelector('#popupProfile');
+new FormValidator(popupNewCard.querySelector('form'), errorMessages);
+new FormValidator(popupProfileNode.querySelector('form'), errorMessages);
+const cardList = new CardList(container);
+const api = new Api({
+    baseUrl: 'https://praktikum.tk/cohort10',
+    headers: {
+        authorization: 'e1b45cd6-f123-45ce-bec1-2b97665df892',
+        'Content-Type': 'application/json'
+    }
+});
+
+// получаем карточки с Api и отрисовываем на странице
+function getInitialCards() {
+    api.getInitialCards()
+        .then(result => {
+            const cards = result.map(item => {
+                const oneCard = new Card(item.name, item.link, item._id, item.likes, userInfo.idUser, item.owner._id, api);
+                return oneCard.create();
+            });
+            cardList.render(cards);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+}
+
+function createCard(name, link, cardId, likeCounter, idUser, idOwner, api) {
+    const newCard = new Card(name, link, cardId, likeCounter, idUser, idOwner, api);
+    cardList.addCard(newCard.create());
+}
+
+const userInfo = new UserInfo(popupProfileNode, [userInfoName, userInfoJob], api, userPhoto);
+
+const popupCard = new PopupCard(popupNewCard, createCard, api);
+new PopupProfile(popupProfileNode, userInfo);
+
+const imageContainer = document.querySelector('#popupImage');
+new PopupImage(imageContainer);
+
+// добавление аватара
+const popupAvatarNode = document.querySelector('#popupAvatar');
+new PopupAvatar(popupAvatarNode, popupCard, api);
+new FormValidator(popupAvatarNode.querySelector('form'), errorMessages);
+
+
+userInfo.getUserInfo();
+getInitialCards();
---
+++ src/js/Card.js
@@ -0,0 +1,87 @@
+export class Card {
+  constructor(name, link, cardId, likeCounter, idUser, idOwner, api) {
+    this.name = name;
+    this.link = link;
+    this.cardId = cardId;
+    this.likeCounter = likeCounter;
+    this.idUser = idUser;
+    this.idOwner = idOwner;
+    this.api = api;
+    this.popupImage = document.querySelector('#popupImage');
+  }
+
+  like(event) {
+    const likeCount = event.target.closest('.place-card__likes').querySelector('.place-card__like-count');
+    if (!event.target.matches('.place-card__like-icon_liked')) {
+      this.api.likeAdd(likeCount.dataset.id)
+          .then(result => {
+            likeCount.textContent = result.likes.length;
+            event.target.classList.add('place-card__like-icon_liked');
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+    } else {
+      this.api.likeDel(likeCount.dataset.id)
+          .then(result => {
+            likeCount.textContent = result.likes.length;
+            event.target.classList.remove('place-card__like-icon_liked');
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+    }
+  }
+
+  remove(event) {
+    if (confirm('Вы действительно хотите удалить эту карточку?')) {
+      const likeCount = event.target.closest('.place-card').querySelector('.place-card__like-count');
+      this.api.cardDel(likeCount.dataset.id)
+          .then(() => {
+            const placeCard = likeCount.closest('.place-card');
+            container.removeChild(placeCard);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+    }
+  }
+
+  create() {
+    const tmpl = document.getElementById('tmpl'); // тег template
+    const fragment = (tmpl.content).cloneNode(true);
+    const placeCardImage = fragment.querySelector('.place-card__image');
+    const placeCardName = fragment.querySelector('.place-card__name');
+    const likeCount = fragment.querySelector('.place-card__like-count');
+    likeCount.setAttribute('data-id', this.cardId);
+    likeCount.textContent = this.likeCounter.length;
+    placeCardImage.style.backgroundImage = `url(${this.link})`;
+    placeCardName.textContent = this.name;
+
+    const likeIcon = fragment.querySelector('.place-card__like-icon');
+    this.likeCounter.forEach(item => {
+      if (item._id === this.idUser) {
+        likeIcon.classList.add('place-card__like-icon_liked');
+      }
+    });
+
+    const delBtn = fragment.querySelector('.place-card__delete-icon');
+    if(this.idOwner === this.idUser) delBtn.style.display = 'block';
+
+    placeCardImage.setAttribute('data-url', this.link);
+    const template = fragment.cloneNode(true);
+    template.querySelector('.place-card__like-icon').addEventListener('click', this.like.bind(this));
+    template.querySelector('.place-card__delete-icon').addEventListener('click', this.remove.bind(this));
+    template.querySelector('.place-card__image').addEventListener('click', this.openImage.bind(this));
+    return template;
+  }
+
+  openImage(event) {
+    if (event.target.matches('.place-card__image')) {
+      const imageSrc = event.target.dataset.url;
+      const popupImg = this.popupImage.querySelector('.popup__img');
+      popupImg.src = imageSrc;
+      this.popupImage.classList.add('popup_is-opened');
+    }
+  }
+}
