66e75c4f583ee0f163c1c06072bcb0df3e9f2ec4
stivman <gisma87@yandex.ru>
2020-09-15 22:58:00 +0700

    change base url

--- src/index.js
+++ src/index.js
@@ -13,9 +13,9 @@ const buttonsMoreCards = document.querySelector('.buttons-nav__show-more-cards')
 const container = document.querySelector('.places-list');
 const userPhoto = document.querySelector('.user-info__photo');
 const errorMessages = {
-    valueMissing: 'Это обязательное поле',
-    tooShort: 'Должно быть от 2 до 30 символов',
-    typeMismatch: 'Здесь должна быть ссылка'
+  valueMissing: 'Это обязательное поле',
+  tooShort: 'Должно быть от 2 до 30 символов',
+  typeMismatch: 'Здесь должна быть ссылка'
 };
 const popupNewCard = document.querySelector('#popupNewPlace');
 const userInfoName = document.querySelector('.user-info__name');
@@ -25,36 +25,36 @@ new FormValidator(popupNewCard.querySelector('form'), errorMessages);
 new FormValidator(popupProfileNode.querySelector('form'), errorMessages);
 const cardList = new CardList(container, buttonsMoreCards);
 const api = new Api({
-    baseUrl: 'https://praktikum.tk/cohort10',
-    headers: {
-        authorization: 'e1b45cd6-f123-45ce-bec1-2b97665df892',
-        'Content-Type': 'application/json'
-    }
+  baseUrl: 'https://nomoreparties.co/cohort10',
+  headers: {
+    authorization: 'e1b45cd6-f123-45ce-bec1-2b97665df892',
+    'Content-Type': 'application/json'
+  }
 });
 
 // получаем карточки с Api и отрисовываем на странице
 function getInitialCards() {
-    api.getInitialCards()
-        .then(result => {
-            // в cardsFilter убираю трёх спамеров, наплодили 50 пустых карточек
-            const cardsFilter = result.filter(item => {
-                return (item.owner._id !== '8e5308053b2657893352cde6' && item.owner._id !== '23a40bc402fd00844fffe55d' && item.owner._id !== '1efaef5b38d89184686eacbe');
-            });
-            const cards = cardsFilter.map(item => {
-                const oneCard = new Card(item.name, item.link, item._id, item.likes, userInfo.idUser, item.owner._id, api);
-                return oneCard.create();
-            });
-            const reverseCards = cards.reverse();
-            cardList.render(reverseCards);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+  api.getInitialCards()
+    .then(result => {
+      // в cardsFilter убираю трёх спамеров, наплодили 50 пустых карточек
+      const cardsFilter = result.filter(item => {
+        return (item.owner._id !== '8e5308053b2657893352cde6' && item.owner._id !== '23a40bc402fd00844fffe55d' && item.owner._id !== '1efaef5b38d89184686eacbe');
+      });
+      const cards = cardsFilter.map(item => {
+        const oneCard = new Card(item.name, item.link, item._id, item.likes, userInfo.idUser, item.owner._id, api);
+        return oneCard.create();
+      });
+      const reverseCards = cards.reverse();
+      cardList.render(reverseCards);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 }
 
 function createCard(name, link, cardId, likeCounter, idUser, idOwner, api) {
-    const newCard = new Card(name, link, cardId, likeCounter, idUser, idOwner, api);
-    cardList.addCard(newCard.create(), 1);
+  const newCard = new Card(name, link, cardId, likeCounter, idUser, idOwner, api);
+  cardList.addCard(newCard.create(), 1);
 }
 
 const userInfo = new UserInfo(popupProfileNode, [userInfoName, userInfoJob], api, userPhoto);
